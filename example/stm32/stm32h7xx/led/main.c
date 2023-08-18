c4afe41e3deccfad5b10678f2d76a31279d3fc28
zhenquan.qiu <gnsyxiang@163.com>
2023-08-18 16:30:08 +0800

    feat: use hy_log.h interface

--- example/stm32/stm32h7xx/led/main.c
+++ example/stm32/stm32h7xx/led/main.c
@@ -21,9 +21,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <hy_log/hy_log.h>
+
+#include <hy_utils/hy_module.h>
+#include <hy_utils/hy_mem.h>
+#include <hy_utils/hy_utils.h>
+
 #include "hy_hal_uart.h"
 
 #include "main.h"
+
+#include "config.h"
 
 typedef struct {
     HyHalUart_s *uart_debug_h;
@@ -143,46 +151,111 @@ void assert_failed(uint8_t *file, uint32_t line)
 }
 #endif /* USE_FULL_ASSERT */
 
+static void _bool_module_destroy(_led_context_s **context_pp)
+{
+    HyModuleDestroyBool_s bool_module[] = {
+        {"log",             HyLogDeInit     },
+    };
+
+    HY_MODULE_RUN_DESTROY_BOOL(bool_module);
+}
+
+static hy_s32_t _bool_module_create(_led_context_s *context)
+{
+    HyLogConfig_s log_c;
+    HY_MEMSET(&log_c, sizeof(log_c));
+    log_c.config_file               = "../res/hy_log/zlog.conf";
+    log_c.fifo_len                  = 10 * 1024;
+    log_c.save_c.level              = HY_LOG_LEVEL_TRACE;
+    log_c.save_c.output_format      = HY_LOG_OUTFORMAT_ALL_NO_PID_ID;
+
+    HyModuleCreateBool_s bool_module[] = {
+        {"log",         &log_c,         (HyModuleCreateBoolCb_t)HyLogInit,          HyLogDeInit},
+    };
+
+    HY_MODULE_RUN_CREATE_BOOL(bool_module);
+}
+
+static void _handle_module_destroy(_led_context_s **context_pp)
+{
+    _led_context_s *context = *context_pp;
+
+    // note: 增加或删除要同步到HyModuleCreateHandle_s中
+    HyModuleDestroyHandle_s module[] = {
+        {"log", (void **)&context->uart_debug_h, (HyModuleDestroyHandleCb_t)HyHalUartDestroy},
+    };
+
+    HY_MODULE_RUN_DESTROY_HANDLE(module);
+}
+
+static hy_s32_t _handle_module_create(_led_context_s *context)
+{
+    HyHalUartConfig_s uart_c;
+    memset(&uart_c, 0, sizeof(uart_c));
+    uart_c.save_c.num   = HY_HAL_UART_NUM_2;
+    uart_c.data_bit     = HY_HAL_UART_DATA_BIT_8;
+    uart_c.parity       = HY_HAL_UART_PARITY_NONE;
+    uart_c.stop_bit     = HY_HAL_UART_STOP_BIT_1;
+    uart_c.rate         = HY_HAL_UART_RATE_115200;
+    uart_c.flow_control = HY_HAL_UART_FLOW_CONTROL_NONE;
+
+    // note: 增加或删除要同步到HyModuleDestroyHandle_s中
+    HyModuleCreateHandle_s module[] = {
+        {"log", (void **)&context->uart_debug_h, &uart_c, (HyModuleCreateHandleCb_t)HyHalUartCreate, (HyModuleDestroyHandleCb_t)HyHalUartDestroy},
+    };
+
+    HY_MODULE_RUN_CREATE_HANDLE(module);
+}
+
 int main(void)
 {
     HAL_Init();
 
     SystemClock_Config();
 
-    do {
-        _led_context_s *context = calloc(1, sizeof(*context));
-        if (!context) {
-            printf("calloc failed \n");
-            break;
-        }
-
-        HyHalUartConfig_s uart_c;
-        memset(&uart_c, 0, sizeof(uart_c));
-        uart_c.save_c.num   = HY_HAL_UART_NUM_2;
-        uart_c.data_bit     = HY_HAL_UART_DATA_BIT_8;
-        uart_c.parity       = HY_HAL_UART_PARITY_NONE;
-        uart_c.stop_bit     = HY_HAL_UART_STOP_BIT_1;
-        uart_c.rate         = HY_HAL_UART_RATE_115200;
-        uart_c.flow_control = HY_HAL_UART_FLOW_CONTROL_NONE;
-        context->uart_debug_h = HyHalUartCreate(&uart_c);
-        if (!context->uart_debug_h) {
-            printf("HyHalUartCreate failed \n");
-            break;
-        }
-
-    } while(0);
-
     MX_GPIO_Init();
 
-    while (1) {
-        printf("-----hello world \n");
+    _led_context_s *context = NULL;
 
+    do {
+        context = HY_MEM_CALLOC_BREAK(_led_context_s *, sizeof(*context));
+
+        struct {
+            const char *name;
+            hy_s32_t (*_create)(_led_context_s *context);
+        } create_arr[] = {
+            {"_bool_module_create",     _bool_module_create},
+            {"_handle_module_create",   _handle_module_create},
+        };
+        for (size_t i = 0; i < HY_UTILS_ARRAY_CNT(create_arr); i++) {
+            if (create_arr[i]._create) {
+                if (0 != create_arr[i]._create(context)) {
+                    LOGE("%s failed \n", create_arr[i].name);
+                }
+            }
+        }
+
+        LOGE("version: %s, data: %s, time: %s \n", VERSION, __DATE__, __TIME__);
+    } while(0);
+
+    while (1) {
         HAL_GPIO_TogglePin(GPIOG, GPIO_PIN_7);
 
         for (size_t i = 0; i < 10000; i++) {
-            for (size_t i = 0; i < 1000; i++) {
+            for (size_t i = 0; i < 10000; i++) {
             }
         }
     }
-}
 
+    void (*destroy_arr[])(_led_context_s **context_pp) = {
+        _handle_module_destroy,
+        _bool_module_destroy
+    };
+    for (size_t i = 0; i < HY_UTILS_ARRAY_CNT(destroy_arr); i++) {
+        if (destroy_arr[i]) {
+            destroy_arr[i](&context);
+        }
+    }
+
+    HY_MEM_FREE_PP(&context);
+}
